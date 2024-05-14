a00d7dc6a81d02981d7a3928a707a77c31c5b68b
eduardo <eduardolopezhernandez@alumno.ieselrincon.es>
2024-05-14 14:29:39 +0100

    Home guapete

--- src/components/house/House.jsx
+++ src/components/house/House.jsx
@@ -3,16 +3,9 @@ import "./House.css";
 function House({ house }) {
   return (
     <div className="house-item">
-
-
       <div className="house-item-img-wrapper">
-        <img src={`${house.src}`} alt="fotodeunCasoplon" />
+       
       </div>
-
-      {/* <div className="house-name">
-        <p>{house.name}</p>
-        <p>See details</p>
-      </div> */}
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import Footer from "../../components/footer/Footer";
 import HeaderImage from "../../components/header-image/HeaderImage";
 import Header from "../../components/header/Header";
 import houses from "../../services/houses/houses.js";
-import House from '../../components/house/House.jsx';
 import './Home.css';
 import Luxury from '../../components/luxury/Luxury.jsx';
 
@@ -16,7 +15,13 @@ function Home() {
       <div className="house-gallery">
         {houses.map((house, index) => (
           <div className="home-house">
-            <House key={index} house={house} />
+            <div className='section'>
+              <p className='house-title'>PROJECT</p>
+              <p className='house-name'>{house.name}</p>
+            </div>
+            <div className='aside'>
+              <img src={`${house.src}`} alt="fotodeunCasoplon" className='house-image' />
+            </div>
           </div>
         ))}
       </div>
